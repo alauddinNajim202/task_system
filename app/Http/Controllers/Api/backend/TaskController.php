06db25a7f8930a90e9d7ff0a90943b128f337e65
Alauddin Najim <alauddinnajim202@gmail.com>
2024-09-21 23:49:46 -0700

    Task api crud created

--- app/Http/Controllers/Api/backend/TaskController.php
+++ app/Http/Controllers/Api/backend/TaskController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Api\backend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
@@ -12,7 +18,28 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        
+        try {
+
+            $tasks = Task::get_all_tasks();
+
+            return response()->json([
+                'success' => true,
+                'tasks' => $tasks
+            ], 201);
+
+
+         } catch (Exception $e) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred while processing your request',
+                'error'   => $e->getMessage()
+            ], 500);
+
+        }
+
+
     }
 
     /**
@@ -28,15 +55,80 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'user_id' => 'required',
+            'category_id' =>'required',
+            'assignee_to' =>'required',
+            
+            'description' =>'required',
+            'status' =>'required',
+            'due_date' =>'required',
+            
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()->first(),
+                'status' => false,
+            ], 400);
+        }
+
+        try {
+
+            $task = Task::create([
+                'name' => $request->name,
+                'user_id' => $request->user_id,
+                'category_id' => $request->category_id,
+                'assignee_to' => $request->assignee_to,
+                'description' => $request->description,
+                'status' => $request->status,
+                'due_date' => $request->due_date,
+                
+            ]);
+
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Task created successfully',
+                'task' => $task
+            ], 201);
+
+               
+           
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred while processing your request',
+                'error'   => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        try {
+
+            $task = Task::get_single_task($id);
+
+            return response()->json([
+                'success' => true,
+                'task' => $task
+            ], 201);
+
+
+         } catch (Exception $e) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred while processing your request',
+                'error'   => $e->getMessage()
+            ], 500);
+
+        }
     }
 
     /**
@@ -50,16 +142,90 @@ class TaskController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request,  $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'user_id' => 'required',
+            'category_id' =>'required',
+            'assignee_to' =>'required',
+            
+            'description' =>'required',
+            'status' =>'required',
+            'due_date' =>'required',
+            
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()->first(),
+                'status' => false,
+            ], 400);
+        }
+
+        try {
+
+            $task = Task::get_single_task($id);
+
+            // task check
+            if(is_null($task)){
+                return response()->json(['message' => 'Task not found'], 404);
+            }
+
+            $task->update([
+                'name' => $request->name,
+                'user_id' => $request->user_id,
+                'category_id' => $request->category_id,
+                'assignee_to' => $request->assignee_to,
+                'description' => $request->description,
+                'status' => $request->status,
+                'due_date' => $request->due_date,
+                
+            ]);
+
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Task updated successfully',
+                'task' => $task
+            ], 201);
+
+               
+           
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred while processing your request',
+                'error'   => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        try {
+
+            $task = Task::get_single_task($id);
+            $task->delete();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Task deleted successfully',
+
+            ], 201);
+
+
+         } catch (Exception $e) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred while processing your request',
+                'error'   => $e->getMessage()
+            ], 500);
+
+        }
     }
 }
